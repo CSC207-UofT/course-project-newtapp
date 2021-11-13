64143a775d15e0d5eb3d8f4f25a8d32a06037f65
wilgagne <william.gagne@mail.utoronto.ca>
2021-11-13 15:14:50 -0500

    BREAKING CHANGE : Changed ConversationAddUser to AddUser. Modified AddUser to fetch conversation and user from database. Added errors accordingly. Made slight changes to ConversationManager.

--- src/main/java/com/newts/newtapp/api/application/ConversationManager.java
+++ src/main/java/com/newts/newtapp/api/application/ConversationManager.java
@@ -1,11 +1,39 @@
 package com.newts.newtapp.api.application;
 
+import com.newts.newtapp.api.ConversationRepository;
+import com.newts.newtapp.api.MessageRepository;
+import com.newts.newtapp.api.UserRepository;
 import com.newts.newtapp.api.application.conversation.*;
+import com.newts.newtapp.api.errors.ConversationFull;
+import com.newts.newtapp.api.errors.ConversationNotFound;
+import com.newts.newtapp.api.errors.UserBelowMinimumRating;
+import com.newts.newtapp.api.errors.UserNotFound;
+import com.newts.newtapp.entities.Conversation;
 
 /**
  * An object representing a ConversationManager of the application.
  */
 public class ConversationManager {
+    /**
+     * The ConversationRepository, MessageRepository and UserRepository this UserManager is working with.
+     */
+    private final ConversationRepository conversationRepository;
+    private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * Initialize a new ConversationManager with given UserRepository.
+     * @param conversationRepository ConversationRepository containing conversation data.
+     * @param messageRepository      MessageRepository containing message data.
+     * @param userRepository         User repository containing user data.
+     */
+    public ConversationManager(ConversationRepository conversationRepository,
+                               MessageRepository messageRepository,
+                               UserRepository userRepository){
+        this.conversationRepository = conversationRepository;
+        this.messageRepository = messageRepository;
+        this.userRepository = userRepository;
+    }
 
     /**
      * Creates a conversation using request
@@ -29,8 +57,8 @@ public class ConversationManager {
      * Adds the user specified by request to the conversation
      * @param request the filled in RequestModel
      */
-    public void addUser(RequestModel request) throws Exception {
-        ConversationAddUser conversationAddUser = new ConversationAddUser();
+    public void addUser(RequestModel request) throws UserNotFound, ConversationNotFound, UserBelowMinimumRating, ConversationFull {
+        AddUser conversationAddUser = new AddUser(conversationRepository, userRepository);
         conversationAddUser.request(request);
     }
 
