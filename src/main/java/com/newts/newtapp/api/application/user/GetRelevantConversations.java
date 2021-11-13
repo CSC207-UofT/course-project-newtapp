f4bba8fde53a716b53090abb442d71a3c223cc3d
Type your name here <alex.szokolay@mail.utoronto.ca>
2021-11-13 14:00:22 -0500

    Implemented GetRelevantConversations.java

--- src/main/java/com/newts/newtapp/api/application/user/GetRelevantConversations.java
+++ src/main/java/com/newts/newtapp/api/application/user/GetRelevantConversations.java
@@ -5,7 +5,6 @@ import com.newts.newtapp.api.UserRepository;
 import com.newts.newtapp.api.application.RequestField;
 import com.newts.newtapp.api.application.RequestModel;
 import com.newts.newtapp.api.application.sorters.InterestSorter;
-import com.newts.newtapp.api.application.user.UserInteractor;
 import com.newts.newtapp.api.application.ConversationQueue;
 import com.newts.newtapp.api.errors.UserNotFound;
 import com.newts.newtapp.entities.Conversation;
@@ -14,7 +13,7 @@ import com.newts.newtapp.entities.User;
 
 import java.util.ArrayList;
 
-public class GetRelevantConversations extends UserInteractor<ArrayList<Conversation>,Exception>  {
+public class GetRelevantConversations extends UserInteractor<Conversation[],UserNotFound>  {
     private UserRepository repository;
     private ConversationRepository conversationRepository;
 
@@ -27,56 +26,22 @@ public class GetRelevantConversations extends UserInteractor<ArrayList<Conversat
     }
 
     @Override
-    public ArrayList<Conversation> request(RequestModel request) throws Exception {
+    public Conversation[] request(RequestModel request) throws UserNotFound {
         int userId = (int) request.get(RequestField.ID);
 
         User user = repository.findById(userId).orElseThrow(UserNotFound::new);
 
         InterestSorter sorter = new InterestSorter();
 
+        ArrayList<String> userInterests = user.getInterests();
+        String userLocation = user.getLocation();
+        int userRadius = (int) request.get(RequestField.LOCATION_RADIUS);
 
+        ConversationQueue conversationQueue = new ConversationQueue(sorter, userLocation, userRadius, userInterests);
 
+        conversationQueue.addAll(conversationRepository.findAll());
 
+        return conversationQueue.toArray();
 
-        return null;
     }
 }
-
-//
-//    @Override
-//    public void request(RequestModel request) {
-//        // Fetching User to generate ConversationQueue for
-//        user = DataBase.getUser((String) request.get(RequestField.USERNAME));
-//
-//        // Create a ConversationQueue with desired settings
-//        InterestSorter sorter = new InterestSorter();
-//        ArrayList<String> interests = new ArrayList<>();
-//        interests.add((String) request.get(RequestField.INTEREST));
-//        conversationQueue = new ConversationQueue(sorter, (String) request.get(RequestField.LOCATION),
-//                (int) request.get(RequestField.LOCATION_RADIUS),
-//                interests);
-//        ResponseModel response = new ResponseModel();
-//
-//        //Get all the conversations in DataBase
-//        conversationQueue.addAll(DataBase.getConversationList());
-//        response.fill(ResponseField.VALUE, conversationQueue.toArray());
-//        // send response through provided output boundary
-//        request.getOutput().respond(response);
-//    }
-//    /**
-//     * Getter method for GetRelevantConversation's conversationQueue.
-//     * @return Returns GetRelevantConversation's conversationQueue.
-//     */
-//    public ConversationQueue getConversationQueue(){
-//
-//        return this.conversationQueue;
-//    }
-//
-//    /**
-//     * Getter method for GetRelevantConversation's user.
-//     * @return Returns GetRelevantConversation's user.
-//     */
-//    public User getUser(){
-//
-//        return this.user;
-//    }
