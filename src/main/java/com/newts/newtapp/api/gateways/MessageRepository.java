b6fb732b3d9d3be84e94cf6f40ad5298afc59c4b
Ted Park <90363387+SCOU7@users.noreply.github.com>
2021-12-04 23:22:29 -0500

    Removed the decorators.

--- src/main/java/com/newts/newtapp/api/gateways/MessageRepository.java
+++ src/main/java/com/newts/newtapp/api/gateways/MessageRepository.java
@@ -2,11 +2,9 @@ package com.newts.newtapp.api.gateways;
 
 import com.newts.newtapp.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * MessageRepository implementation is handled by Spring Boot.
  */
-@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 }
