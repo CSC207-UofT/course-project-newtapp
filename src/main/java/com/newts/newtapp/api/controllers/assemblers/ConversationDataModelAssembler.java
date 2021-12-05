869b7da32c2966ba619de867418e0e987c1ec910
Ted Park <90363387+SCOU7@users.noreply.github.com>
2021-12-04 23:47:25 -0500

    ConversationDataModelAssembler got cleaner by relocating the EntityModel turn process to GetConversationData

--- src/main/java/com/newts/newtapp/api/controllers/assemblers/ConversationDataModelAssembler.java
+++ src/main/java/com/newts/newtapp/api/controllers/assemblers/ConversationDataModelAssembler.java
@@ -1,8 +1,6 @@
 package com.newts.newtapp.api.controllers.assemblers;
 
 import com.newts.newtapp.api.application.datatransfer.ConversationData;
-import com.newts.newtapp.api.application.datatransfer.MessageData;
-import com.newts.newtapp.api.application.datatransfer.UserProfile;
 import com.newts.newtapp.api.controllers.ConversationController;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
@@ -19,24 +17,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
  * A RepresentationModelAssembler for ConversationData. Implemented to reduce duplicate code.
  */
 @Component
-public class ConversationDataModelAssembler
-        implements RepresentationModelAssembler<ConversationData,
-        EntityModel<ConversationDataWithLink>> {
-
-    private final UserProfileModelAssembler profileAssembler;
-    private final MessageDataModelAssembler messageAssembler;
-
-    public ConversationDataModelAssembler(UserProfileModelAssembler profileAssembler, MessageDataModelAssembler messageAssembler) {
-        this.profileAssembler = profileAssembler;
-        this.messageAssembler = messageAssembler;
-    }
+public class ConversationDataModelAssembler implements RepresentationModelAssembler<ConversationData,
+        EntityModel<ConversationData>> {
 
     /**
      * @param data      ConversationData to be contained in EntityModel
      * @return          EntityModel containing provided ConversationData and appropriate links
      */
     @Override
-    public @NonNull EntityModel<ConversationDataWithLink> toModel(@NonNull ConversationData data) {
+    public @NonNull EntityModel<ConversationData> toModel(@NonNull ConversationData data) {
         ArrayList<Link> links = new ArrayList<>();
         try {
             // Add appropriate links from this ConversationData
@@ -45,20 +34,6 @@ public class ConversationDataModelAssembler
             // No exception will ever be thrown at this point in normal use of this class as an exception would have
             // been thrown earlier during application logic.
         }
-
-        ArrayList<EntityModel> userProfiles = new ArrayList<EntityModel>();
-        ArrayList<EntityModel> messageDatas = new ArrayList<EntityModel>();
-
-        for (UserProfile profile: data.userProfiles) {
-            userProfiles.add(profileAssembler.toModel(profile));
-        }
-
-        for (MessageData message: data.messageData) {
-            messageDatas.add(messageAssembler.toModel(message));
-        }
-
-        return EntityModel.of(new ConversationDataWithLink(messageDatas, userProfiles, data), links);
+        return EntityModel.of(data, links);
     }
-
-
 }
