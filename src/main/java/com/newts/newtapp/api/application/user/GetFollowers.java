8cd865924a3c643ec901fffdc99be6c80ddf91d0
Yukthiw <59380056+Yukthiw@users.noreply.github.com>
2021-12-06 15:03:34 -0500

    Created GetFollowers, GetFollowing and Unblock interactors, along with new UserNotBlocked exception

---
+++ src/main/java/com/newts/newtapp/api/application/user/GetFollowers.java
@@ -0,0 +1,32 @@
+package com.newts.newtapp.api.application.user;
+
+import com.newts.newtapp.api.application.boundary.RequestField;
+import com.newts.newtapp.api.application.boundary.RequestModel;
+import com.newts.newtapp.api.errors.UserNotFound;
+import com.newts.newtapp.api.gateways.UserRepository;
+import com.newts.newtapp.entities.User;
+
+import java.util.ArrayList;
+
+public class GetFollowers extends UserInteractor<ArrayList<Integer>, Exception> {
+
+    /**
+     * Creating GetFollowers UserInteractor which returns an ArrayList of the User's Follower ids
+     * @param userRepository UserRepository for User data access
+     */
+    public GetFollowers(UserRepository userRepository){
+        super(userRepository);
+    }
+
+    /**
+     * Accepts a request.
+     *
+     * @param request a request stored as a RequestModel
+     */
+    @Override
+    public ArrayList<Integer> request(RequestModel request) throws UserNotFound {
+        int userId = (int) request.get(RequestField.USER_ID);
+        User user = userRepository.findById(userId).orElseThrow(UserNotFound::new);
+        return user.getFollowers();
+    }
+}
